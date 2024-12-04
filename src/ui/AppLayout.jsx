361292e922971a6134bc64ec4cc4b6ed62fa2958
Vova <podolian@gmail.com>
2024-12-04 21:37:23 +0200

    Loader and isLoading

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,13 +3,20 @@ import React from "react";
 
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
-      <main><Outlet /></main>
+      <main>
+        <Outlet />
+      </main>
       <CartOverview />
     </div>
   );
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Loader() {
+  return <div className="loader"></div>;
+}
+
+export default Loader;
