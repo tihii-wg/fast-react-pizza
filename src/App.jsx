cfde8ee34faa6d7f533a71cde22ec247abdfba29
tihii-wg <p0d0lian@mail.ru>
2024-12-16 21:03:42 +0200

    add action

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
@@ -32,7 +34,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
         loader: orderLoader,
       },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
     ],
   },
 ]);
