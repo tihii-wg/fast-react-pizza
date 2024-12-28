4ca3af89d622daabf34e5da2798f9109f13b0978
tihii-wg <p0d0lian@mail.ru>
2024-12-28 16:31:00 +0200

    config font and hight

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
-import SerchOrder from './SerchOrder';
-import UserName from '../features/user/UserName';
+import { Link } from "react-router-dom";
+import SerchOrder from "./SerchOrder";
+import UserName from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-3 py-4 uppercase sm:px-8">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-3 py-4 font-sans uppercase sm:px-8">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
