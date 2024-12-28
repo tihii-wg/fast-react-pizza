3743b6dfc86ba51f8470b324d38a983e503d519f
tihii-wg <p0d0lian@mail.ru>
2024-12-28 14:39:49 +0200

    styling loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React from "react";
 
-import Header from './Header';
-import CartOverview from '../features/cart/CartOverview';
-import { Outlet, useNavigation } from 'react-router-dom';
-import Loader from './Loader';
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === 'loading';
+  const isLoading = navigation.state === "loading";
 
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
