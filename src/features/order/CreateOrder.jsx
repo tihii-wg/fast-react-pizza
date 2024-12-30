95f16e8bfdc1fda10c144a1853df13fecf279bf2
tihii-wg <p0d0lian@mail.ru>
2024-12-30 11:20:54 +0200

    styling order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,19 +2,12 @@
 import React from "react";
 
 import { createOrder } from "../../services/apiRestaurant";
-import {
-  Form,
-  redirect,
-  useActionData,
-  useNavigation,
-} from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import Button from "../../ui/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
 const fakeCart = [
   {
@@ -48,64 +41,49 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="post">
-        <div>
-          <label>First Name</label>
-          <input
-            className="input"
-            type="text"
-            name="customer"
-            required
-          />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input className="input grow" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input
-              className="input"
-              type="tel"
-              name="phone"
-              required
-            />
-          </div>
-          {formErrors && formErrors.phone}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input
-              className="input"
-              type="text"
-              name="address"
-              required
-            />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors && (
+              <p className="mt-2 rounded-full bg-red-100 px-4 py-2.5 text-xs text-red-600 sm:py-3">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input className="input w-full" type="text" name="address" required />
+          </div>
+        </div>
+
+        <div className="mb-12 flex items-center gap-5">
           <input
-            className="h-5 w-5 accent-yellow-400 focus:outline-none focus:ring focus:ring-offset-2"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">
+          <label className="font-medium" htmlFor="priority">
             Want to yo give your order priority?
           </label>
         </div>
         <div>
-          <input
-            type="hidden"
-            name="cart"
-            value={JSON.stringify(cart)}
-          />
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button type="primary" disabled={isSubmitting}>
             {isSubmitting ? "Processing.." : "Order now"}
           </Button>
@@ -124,8 +102,7 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
   const errors = {};
-  if (!isValidPhone(order.phone))
-    errors.phone = "Must be a valid number";
+  if (!isValidPhone(order.phone)) errors.phone = "Must be a valid number for getting yuor otder!";
 
   if (Object.keys(errors).length > 0) return errors;
 
