b24d99e55925052e266080a32551ab1f0dd6456f
tihii-wg <p0d0lian@mail.ru>
2025-01-08 12:41:00 +0200

    fetchAddress in userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,36 +1,59 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
+
 function getPosition() {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 }
 
-async function fetchAddress() {
-  // 1) We get the user's geolocation position
-  const positionObj = await getPosition();
-  const position = {
-    latitude: positionObj.coords.latitude,
-    longitude: positionObj.coords.longitude,
-  };
+const fetchAddress = createAsyncThunk(
+  "user/fetchAddress",
+  async function () {
+    // 1) We get the user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
 
-  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-  const addressObj = await getAddress(position);
-  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+    // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
-  // 3) Then we return an object with the data that we are interested in
-  return { position, address };
-}
-
-import { createSlice } from "@reduxjs/toolkit";
+    // 3) Then we return an object with the data that we are interested in
+    return { position, address };
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState: {
     userName: "",
+    loading: "idle",
+    positon: {},
+    address: "",
+    error: "",
   },
   reducers: {
     updateName: (state, action) => {
       state.userName = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.loading = "loading";
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        (state.positon = action.payload.position),
+          (state.address = action.payload.address);
+        state.loading = "idle";
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        state.loading = "error";
+        state.error = action.error.message;
+      });
   },
 });
 
